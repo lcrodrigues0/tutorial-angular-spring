e00b2bf1d3ae883d7da1192f9a7865f7d747d3b3
lcrodrigues0 <leticia.c.rodrigues0@gmail.com>
2025-05-08 10:14:34 -0300

    feat: table and get from json

---
+++ src/app/courses/courses.module.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+
+import { AppMaterialModule } from '../share/app-material/app-material.module';
+import { CoursesRoutingModule } from './courses-routing.module';
+import { CoursesComponent } from './courses/courses.component';
+
+
+@NgModule({
+  declarations: [
+    CoursesComponent
+  ],
+  imports: [
+    CommonModule,
+    CoursesRoutingModule,
+    AppMaterialModule,
+    HttpClientModule
+  ]
+})
+export class CoursesModule { }
---
+++ src/app/courses/courses/courses.component.ts
@@ -0,0 +1,23 @@
+import { Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+
+import { Course } from '../model/course';
+import { CoursesService } from './../services/courses.service';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.scss']
+})
+export class CoursesComponent implements OnInit {
+
+  courses: Observable<Course[]>;
+  displayedColumns = ['name', 'category'];
+
+  constructor(private coursesService: CoursesService) {
+    this.courses = this.coursesService.list();
+  }
+
+  ngOnInit(): void {}
+
+}
